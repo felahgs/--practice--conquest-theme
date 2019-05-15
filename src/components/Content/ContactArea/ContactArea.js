ec2306819c8a17b90188e10eba74ca164bbd343c
Felipe Garcia de Souza <f.souza@cinq.com.br>
2019-05-15 11:37:23 -0300

    added parallax effect to the background images

--- src/components/Content/ContactArea/ContactArea.js
+++ src/components/Content/ContactArea/ContactArea.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Form from '../Form/Form';
-import Image from '../../../assets/images/home-image.jpg'
+import Image from '../../../assets/images/contact/chinese_moutain_peek.jpg'
 
 import './ContactArea.css';
 
 const contactArea = () => {
     return (
         // <div className="contact-area" style={{backgroundImage: 'url(' + Image + ')'}}>
-        <div className="contact-area" >
+        <div className="contact-area" style ={ { backgroundImage: "url(" + Image + ")" }} >
             {/* <h1>Contact Area</h1> */}
             <div className="container">
                 <div className="contact-message" >
